8aebd9d8919843288693661b7c50c26b8137f938
Haoyu Yu <hyu@nyc-mbp-hyu.schrodinger.com>
2019-03-01 17:58:18 -0500

    all components

---
+++ notes.js
@@ -0,0 +1,29 @@
+/*
+1. Redux for state manager
+2. CLI tool for creating reaction applications
+    use webpack(module bundler)
+3. Anatomy of a component
+
+Class Post extends from React.Component{
+    super()
+    state = {
+        title: 'Post One',
+        body: 'This is my post'
+    }
+    render(){     // this return JSX-> like html
+        return (
+            <div>
+                <h3>{ this.state.title }</h3>   // can be any js script
+                <p>{ this.state.body }</p>
+            </div>
+        )
+    }
+}
+   
+A page has multiple component
+
+
+
+
+*/ 
+
